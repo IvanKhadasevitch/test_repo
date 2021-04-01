7201fe8df3d6a463ca89639e64b4a8c64ca81379
Ivan Khadasevich <adv.club.ivan@gmail.com>
2021-04-01 21:11:32 +0300

    fix issues in PR

--- utils/src/main/java/by/khadasevich/utils/StringUtils.java
+++ utils/src/main/java/by/khadasevich/utils/StringUtils.java
@@ -1,15 +1,15 @@
 package by.khadasevich.utils;
 
-import static org.apache.commons.lang3.math.NumberUtils.*;
+import static org.apache.commons.lang3.math.NumberUtils.createDouble;
+import static org.apache.commons.lang3.math.NumberUtils.isCreatable;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         if (isCreatable(str)) {
             Double number = createDouble(str);
-            if (number > 0) {
-                return true;
-            }
+            return number > 0;
+        } else {
+            throw new IllegalArgumentException("Not a number format");
         }
-        return false;
     }
 }
--- utils/src/test/java/by/khadasevich/utils/StringUtilsTest.java
+++ utils/src/test/java/by/khadasevich/utils/StringUtilsTest.java
@@ -1,22 +1,36 @@
 package by.khadasevich.utils;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StringUtilsTest {
 
-    @Test
-    void isPositiveNumber() {
-        assertTrue(StringUtils.isPositiveNumber("1f"));
-        assertTrue(StringUtils.isPositiveNumber("3"));
-        assertTrue(StringUtils.isPositiveNumber("3"));
+    @ParameterizedTest
+    @ValueSource(strings = {"1f", "3", "3.2"})
+    void isPositiveNumberTrue(String value) {
+        assertTrue(StringUtils.isPositiveNumber(value));
+    }
 
-        assertFalse(StringUtils.isPositiveNumber("0"));
-        assertFalse(StringUtils.isPositiveNumber("-1.2"));
-        assertFalse(StringUtils.isPositiveNumber("-1f"));
-        assertFalse(StringUtils.isPositiveNumber(""));
-        assertFalse(StringUtils.isPositiveNumber(null));
-        assertFalse(StringUtils.isPositiveNumber("string"));
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1.2", "-1f"})
+    void isPositiveNumberFalse(String value) {
+        assertFalse(StringUtils.isPositiveNumber(value));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", " ", " 3", "string"})
+    void isPositiveNumberException(String value) {
+        //IllegalArgumentException is parent class of NumberFormatException
+        //This assertion will pass
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            StringUtils.isPositiveNumber(value);
+        });
+
     }
 }
